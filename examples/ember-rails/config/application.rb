1fc70497a8cc4418587e588eea58da267934e32e
Joe Fiorini <joe@joefiorini.com>
2014-02-28 10:27:23 -0500

    Setup bower

--- examples/ember-rails/config/application.rb
+++ examples/ember-rails/config/application.rb
@@ -13,6 +13,10 @@ Bundler.require(:default, Rails.env)
 
 module DeviseSimpleAuthExample
   class Application < Rails::Application
+    bower_dir = Rails.root.join('vendor', 'assets', 'bower_components').to_s
+    config.assets.paths << bower_dir
+    config.sass.load_paths << bower_dir
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
