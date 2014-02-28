dfebbea275b823aac52623b4a4e835df9ff8fcfa
Joe Fiorini <joe@joefiorini.com>
2014-02-28 10:04:54 -0500

    Generate code for example ember-rails app

---
+++ examples/ember-rails/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeviseSimpleAuthExample::Application.config.secret_key_base = '14f8f92107c1a127c3fef27d84cd46f1c4dac72aa2c398dec8f36a84ee0c34cb5917966a8d8fb28715cadb10f7348118bfde2a1836f16090828ae40d9a986a2b'
---
+++ examples/ember-rails/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DeviseSimpleAuthExample::Application.config.session_store :cookie_store, key: '_devise_simple_auth_example_session'
