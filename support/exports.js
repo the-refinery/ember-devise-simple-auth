baa095c56ee0f0926d307c9e3199ad48aa7d1477
Joe Fiorini <joe@joefiorini.com>
2014-02-28 14:50:38 -0500

    Export SessionRoute as global

--- support/exports.js
+++ support/exports.js
@@ -1,6 +1,9 @@
-Ember.DeviseSimpleAuth = require('ember-devise-simple-auth');
+Ember.DeviseSimpleAuth = Ember.Namespace.create();
+
+require('ember-devise-simple-auth');
 
 var SessionRoute = require('app/routes/session').default;
+Ember.DeviseSimpleAuth.SessionRoute = SessionRoute;
 
 Ember.Application.initializer({
   name: "register-session-route",
