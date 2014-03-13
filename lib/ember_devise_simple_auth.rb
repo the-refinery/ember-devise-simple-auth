6b0ccc5c392082efde7393f9a5a7438c9fb05894
Joe Fiorini <joe@joefiorini.com>
2014-03-13 14:31:56 -0400

    Fix placement of gem require

--- lib/ember_devise_simple_auth.rb
+++ lib/ember_devise_simple_auth.rb
@@ -1,7 +1,6 @@
 $: << File.expand_path(File.join(__FILE__, '../../support/rails/lib'))
 
-require "ember_devise_simple_auth/engine"
-
 module EmberDeviseSimpleAuth
 end
 
+require "ember_devise_simple_auth/engine"
