26a430246c27b5cacff5512b6e85dbadbba80cdb
Joe Fiorini <joe@joefiorini.com>
2014-03-13 14:05:49 -0400

    Improve setup of supporting gem

---
+++ lib/ember_devise_simple_auth.rb
@@ -0,0 +1,7 @@
+$: << File.expand_path(File.join(__FILE__, '../../support/rails/lib'))
+
+require "ember_devise_simple_auth/engine"
+
+module EmberDeviseSimpleAuth
+end
+
--- support/rails/lib/ember_devise_simple_auth/version.rb
+++ support/rails/lib/ember_devise_simple_auth/version.rb
@@ -1,7 +1,7 @@
 require 'json'
 module EmberDeviseSimpleAuth
   def self.version_from_package_json
-    pkg = File.read(File.expand_path("dist/bower.json").tap{|u| puts u.inspect})
+    pkg = File.read(File.expand_path("dist/bower.json")
     JSON.parse(pkg)["version"]
   end
 
